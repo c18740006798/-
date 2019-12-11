eb3ae3bd95cac7da18ccd599c8945c749830bb70
chen <358201290@qq.com>
2019-12-11 12:33:31 +0800

    m

--- 1.js
+++
@@ -1,8 +0,0 @@
- const http = require("http")
- let server = http.createServer((req,res)=>{
-     res.write("hahaha")
-     res.end("heloworlld")
- })
- server.on('request',(req,res)=>{
-     console.log("hello,wrold")
- })
